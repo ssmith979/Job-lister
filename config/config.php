e77727e76fcc4805ec0642e2d20f34df8028f076
Susanna Smith <50603908+ssmith979@users.noreply.github.com>
2020-10-26 13:22:05 -0400

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php 
+//DB Params
+define("DB_HOST","localhost");
+define("DB_USER", 'root');
+define('DB_PASS','Lox4Life!');
+define('DB_NAME', 'Joblister');
+
+define('SITE_TITLE', 'JobLister');
